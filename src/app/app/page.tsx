c97f63d4c3f1560f68e32da2bdb9eff0c8e3f89c
g-ouattara <idrissgouattara@gmail.com>
2023-11-11 11:31:18 +0000

    removed bottomsheet here

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,21 +1,14 @@
 'use client';
 
+import Bottomsheet from "@/components/bottomsheet";
 import { useState } from "react";
-import { BottomSheet } from "react-spring-bottom-sheet";
-import "./bottomsheet.css";
-// import 'react-spring-bottom-sheet/dist/style.css';
-
 
 export default function Home() {
     const [open, setOpen] = useState(false)
     return (
-        <>
-            <button onClick={() => setOpen(true)}>Open Me</button>
-            <BottomSheet
-                blocking={false}
-                onDismiss={() => setOpen(false)}
-                snapPoints={({ minHeight, maxHeight }) => [minHeight, maxHeight]}
-                open={open}>My awesome content here</BottomSheet>
-        </>
+        <main>
+            <h1>Hello World</h1>
+            <Bottomsheet />
+        </main>
     )
 }
