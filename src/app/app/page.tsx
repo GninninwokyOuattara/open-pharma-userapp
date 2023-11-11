7f3aba331727ed6d2e0f8a52957fa8c2ad48e14b
g-ouattara <idrissgouattara@gmail.com>
2023-11-11 10:08:31 +0000

    basic of the app page

---
+++ src/app/app/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useState } from "react";
+import { BottomSheet } from "react-spring-bottom-sheet";
+import 'react-spring-bottom-sheet/dist/style.css';
+
+
+export default function Home() {
+    const [open, setOpen] = useState(false)
+    return (
+        <>
+            <button onClick={() => setOpen(true)}>Open Me</button>
+            <BottomSheet
+                onDismiss={() => setOpen(false)}
+                snapPoints={({ minHeight, maxHeight }) => [minHeight, maxHeight]}
+                open={open}>My awesome content here</BottomSheet>
+        </>
+    )
+}
