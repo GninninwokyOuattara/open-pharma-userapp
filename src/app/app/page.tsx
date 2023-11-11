ead10f5331ad26d501d7159245da6d4d6e2472aa
g-ouattara <idrissgouattara@gmail.com>
2023-11-11 16:11:33 +0000

    little padding on medium size screen

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -8,12 +8,12 @@ import LeafletMap from "@/components/leafletMap";
 export default function Home() {
     return (
         <main
-            className="h-screen w-screen border-2 border-red-500"
+            className="h-screen w-screen"
         >
             <div id="mainContainer"
                 className="flex flex-column h-full">
-                <aside className="hidden md:flex w-[40%] max-w-[500px] p-2 border-2 border-red-500 h-full">Data </aside>
-                <div className=" flex-1 h-full z-10">
+                <aside className="hidden md:flex w-[40%] max-w-[500px] p-2 h-full">Data </aside>
+                <div className=" flex-1 h-full md:p-2">
                     <LeafletMap />
                 </div>
             </div>
