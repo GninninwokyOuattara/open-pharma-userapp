cbe54b8e128c371bf91b76d740b40ce71b7bb367
g-ouattara <idrissgouattara@gmail.com>
2023-11-12 12:53:56 +0000

    add the Ping component

--- src/components/pharmacyItem.tsx
+++ src/components/pharmacyItem.tsx
@@ -1,13 +1,18 @@
+import { Pharmacy } from "@/types"
+import Ping from "./ping"
 
+interface Props {
+    pharmacy: Pharmacy
+}
 const PharmacyItem = () => {
     return (
         <div
-            className="border-b-2 py-2  flex flex-col gap-2"
+            className="border-b-2 py-4  flex flex-col gap-2"
         >
-            <div className="flex flex-row gap-2">
+            <div className="flex flex-row gap-2 items-center">
                 <p className="font-medium flex-auto">Pharmacie sainte odile</p>
                 <div className="text-gray-500 font-medium w-20 ">5 mètres</div>
-
+                <Ping />
             </div>
 
         </div>
