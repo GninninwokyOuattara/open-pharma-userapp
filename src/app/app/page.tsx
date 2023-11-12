b43a954ea91efff5db34ce52bfe1f09c29b713e6
g-ouattara <idrissgouattara@gmail.com>
2023-11-12 18:19:25 +0000

    create to custom client component for page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,37 +1,16 @@
-'use client';
 
-import Bottomsheet from "@/components/bottomsheet";
+import AppPageContainer from "@/components/appPageContainer";
 import { getPharmacies } from "@/queries/getPharmacies";
-import dynamic from "next/dynamic";
-import { useMemo } from "react";
 
 
 export default async function Home() {
-
-
-    const LeafletMap = useMemo(() => dynamic(
-        () => import('@/components/leafletMap'),
-        {
-            loading: () => <p>Loading...</p>,
-            ssr: false
-        }
-    ), [])
-
     const initialData = await getPharmacies()
 
     return (
         <main
             className="h-screen w-screen"
         >
-            <div id="mainContainer"
-                className="flex flex-column h-full">
-                <aside className="hidden md:flex w-[40%] max-w-[500px] p-2 h-full">Data </aside>
-                <div className=" flex-1 h-full md:p-2">
-                    <LeafletMap />
-                </div>
-            </div>
-            <Bottomsheet
-                pharmacies={initialData} />
+            <AppPageContainer pharmacies={initialData} />
         </main>
     )
 }
---
+++ src/components/appPageContainer.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Pharmacy } from '@/types';
+import dynamic from 'next/dynamic';
+import React, { useMemo } from 'react';
+import Bottomsheet from './bottomsheet';
+
+interface Props {
+    pharmacies: Pharmacy[]
+}
+
+const AppPageContainer: React.FC<Props> = ({ pharmacies }) => {
+
+    const LeafletMap = useMemo(() => dynamic(
+        () => import('@/components/leafletMap'),
+        {
+            loading: () => <p>Loading...</p>,
+            ssr: false
+        }
+    ), [])
+    return (
+        <>
+            <div id="mainContainer"
+                className="flex flex-column h-full">
+                <aside className="hidden md:flex w-[40%] max-w-[500px] p-2 h-full">Data </aside>
+                <div className=" flex-1 h-full md:p-2">
+                    <LeafletMap />
+                </div>
+            </div>
+            <Bottomsheet
+                pharmacies={pharmacies} />
+        </>
+    )
+}
+
+export default AppPageContainer
