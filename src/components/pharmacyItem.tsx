752f1a0bf9bce325fe1cbfb34a5320cdfdc06416
g-ouattara <idrissgouattara@gmail.com>
2023-11-12 18:13:56 +0000

    styling and show ping if pharmacy open

--- src/components/pharmacyItem.tsx
+++ src/components/pharmacyItem.tsx
@@ -4,15 +4,19 @@ import Ping from "./ping"
 interface Props {
     pharmacy: Pharmacy
 }
-const PharmacyItem = () => {
+const PharmacyItem: React.FC<Props> = ({ pharmacy }) => {
     return (
         <div
             className="border-b-2 py-4  flex flex-col gap-2"
         >
             <div className="flex flex-row gap-2 items-center">
-                <p className="font-medium flex-auto">Pharmacie sainte odile</p>
-                <div className="text-gray-500 font-medium w-20 ">5 mètres</div>
-                <Ping />
+                <p className="font-medium flex-auto">{pharmacy.name}</p>
+                <div className="flex flex-row items-center">
+                    <div className="text-gray-500 font-medium w-20 ">5 mètres</div>
+
+                    {pharmacy.open ? <Ping /> : <div className="w-1 h-2"></div>}
+
+                </div>
             </div>
 
         </div>
