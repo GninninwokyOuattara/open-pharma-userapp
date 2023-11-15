771aaef73639f285b0a65373f45269710f2a2742
g-ouattara <idrissgouattara@gmail.com>
2023-11-15 17:49:57 +0000

    processing pharmacies datas

---
+++ src/app/utils/pharmacies-processors.ts
@@ -0,0 +1,28 @@
+import { Coordinates, Pharmacy, PharmacyWithDistanceToUser } from "@/types";
+import {
+  calculateDistance,
+  convertDistanceToHumanReadable,
+} from "./geo-operation";
+
+export const getDistanceToUser = (
+  pharmacy: Pharmacy,
+  userPosition: Coordinates
+): PharmacyWithDistanceToUser => {
+  try {
+    const distanceRaw = calculateDistance(userPosition, pharmacy.coordinates);
+    const distanceFormatted = convertDistanceToHumanReadable(distanceRaw);
+
+    return {
+      ...pharmacy,
+      distanceRaw,
+      distanceFormatted,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPharmaciesWithDistanceToUser = (
+  pharmacies: Pharmacy[],
+  userPosition: Coordinates
+) => pharmacies.map((pharmacy) => getDistanceToUser(pharmacy, userPosition));
