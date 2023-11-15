30da462d124c2f3b75671642df3a3143d38f4cd0
g-ouattara <idrissgouattara@gmail.com>
2023-11-15 16:36:50 +0000

    can retrieve user location now

--- src/hooks/useUserLocation.tsx
+++ src/hooks/useUserLocation.tsx
@@ -5,8 +5,7 @@ const useUserLocation = () => {
     const [location, setLocation] = useState<{ latitude: number, longitude: number } | null>(null);
     const [error, setError] = useState("");
 
-    useEffect(() => {
-        // Check if geolocation is supported by the browser
+    const getUserLocation = () => {
         console.log("Effect running")
         if ('geolocation' in navigator) {
             console.log("We got geolocation")
@@ -28,6 +27,37 @@ const useUserLocation = () => {
             console.log("We don't have geolocation")
             setError('Geolocation is not supported by your browser.');
         }
+    }
+
+    useEffect(() => {
+
+        const getUserLocation = () => {
+            console.log("Trying to get user location")
+            if ('geolocation' in navigator) {
+                console.log("We got geolocation")
+                console.log("Navigator", navigator)
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        console.log("position", position)
+                        // Success callback
+                        const { latitude, longitude } = position.coords;
+                        setLocation({ latitude, longitude });
+                    },
+                    (error) => {
+                        console.log("error", error)
+                        // Error callback
+                        setError(error.message);
+                    }
+                );
+            } else {
+                console.log("We don't have geolocation")
+                setError('Geolocation is not supported by your browser.');
+            }
+        }
+
+        getUserLocation()
+
+
     }, []);
 
     return {
