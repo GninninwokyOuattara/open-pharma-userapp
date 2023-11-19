479a9ae6b85f8b59dee301c91b9024963de67397
g-ouattara <idrissgouattara@gmail.com>
2023-11-19 00:14:58 +0000

    styling the hover effect

--- src/components/pharmacyItem.tsx
+++ src/components/pharmacyItem.tsx
@@ -8,18 +8,21 @@ interface Props {
 const PharmacyItem: React.FC<Props> = ({ pharmacy, onClick }) => {
     return (
         <div
-            className="border-b-2 py-4  flex flex-col gap-2"
+            className="border-b-2   flex flex-col gap-2 py-0"
             onClick={onClick}
         >
-            <div className="flex flex-row gap-2 items-center">
-                <p className="font-medium flex-auto">{pharmacy.name}</p>
-                <div className="flex flex-row items-center gap-2">
-                    {
-                        'distanceFormatted' in pharmacy && <div className="text-gray-500 font-medium w-24 text-right">{pharmacy.distanceFormatted}</div>
-                    }
+            <div className="hover:bg-orange-50 cursor-pointer px-2 rounded-md h-full py-4">
 
-                    {pharmacy.open ? <Ping /> : <div className="w-1 h-2"></div>}
+                <div className="flex flex-row gap-2 items-center">
+                    <p className="font-medium flex-auto">{pharmacy.name}</p>
+                    <div className="flex flex-row items-center gap-2">
+                        {
+                            'distanceFormatted' in pharmacy && <div className="text-gray-500 font-medium w-24 text-right">{pharmacy.distanceFormatted}</div>
+                        }
 
+                        {pharmacy.open ? <Ping /> : <div className="w-1 h-2"></div>}
+
+                    </div>
                 </div>
             </div>
 
