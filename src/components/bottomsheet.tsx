bcc062980388900cf5ebd6afd9582f6f0b1bd193
g-ouattara <idrissgouattara@gmail.com>
2023-11-19 02:29:58 +0000

    performance improvement for the heavy list

--- src/components/bottomsheet.tsx
+++ src/components/bottomsheet.tsx
@@ -4,6 +4,7 @@ import { useLeaflet } from "@/app/contexts/leafletContext";
 import "@/styles/bottomsheet.style.css";
 import { Pharmacy } from "@/types";
 import { BottomSheet } from "react-spring-bottom-sheet";
+import { VariableSizeList as List } from 'react-window';
 import PharmacyItem from "./pharmacyItem";
 
 
@@ -15,6 +16,23 @@ const Bottomsheet: React.FC<Props> = ({ pharmacies }) => {
 
     const { leafletMapRef } = useLeaflet();
 
+    const pharmaciesItemsRenderer = ({ index, style }: { index: number, style: any }) => {
+
+        const pharmacy = pharmacies[index]
+        return (
+            <div style={style}>
+
+                <PharmacyItem
+                    key={pharmacy.id}
+                    pharmacy={pharmacy}
+                    onClick={() => {
+                        leafletMapRef.current?.flyTo([pharmacy.coordinates.latitude, pharmacy.coordinates.longitude], 15)
+                    }}
+                />
+            </div>
+        )
+    }
+
     return (
         <>
             <BottomSheet
@@ -25,19 +43,18 @@ const Bottomsheet: React.FC<Props> = ({ pharmacies }) => {
                 snapPoints={({ minHeight, maxHeight }) => [25, maxHeight / 2.5, maxHeight]}
                 open>
                 <div className="p-2">
-                    {
-                        pharmacies.map((pharmacy) => {
-                            return (
-                                <PharmacyItem
-                                    key={pharmacy.id}
-                                    pharmacy={pharmacy}
-                                    onClick={() => {
-                                        leafletMapRef.current?.flyTo([pharmacy.coordinates.latitude, pharmacy.coordinates.longitude], 15)
-                                    }}
-                                />
-                            )
-                        })
-                    }
+
+                    <List
+                        height={1000}
+                        itemCount={pharmacies.length}
+                        itemSize={() => 60}
+                        itemData={pharmacies}
+                        width={"full"}
+                    >
+                        {pharmaciesItemsRenderer}
+
+                    </List>
+
                 </div>
             </BottomSheet>
         </>
